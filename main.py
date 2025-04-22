b7593f0e23aca45c266601085c6e429a2421feea
Enrico-Programador <enrico2015ap@gmail.com>
2025-04-22 20:32:37 -0300

    Version 0.0.2

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        
+        #close the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
